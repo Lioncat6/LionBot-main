e4eb202ba488c5f3a8ed5720023556e5fdde59ca
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2024-08-14 13:14:49 -0500

    this

--- events/ready.js
+++ events/ready.js
@@ -4,15 +4,18 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-        client.user.setPresence({
-  			activities: [{ name: `you use /help`, type: ActivityType.Watching }],
-  			status: 'online',
-		});
+        
         const Guilds = [];
+		let numGuilds = 0
 		client.guilds.cache.forEach(guild => {
 			Guilds.push([guild.id, guild.name, guild.memberCount]);
+			numGuilds = numGuilds+1
 		});
 
+		client.user.setPresence({
+			activities: [{ name: `you use /help | ${numGuilds} Servers`, type: ActivityType.Watching }],
+			status: 'online',
+	 	 });
 
         console.log(Guilds)
 	},
